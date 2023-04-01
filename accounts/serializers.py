cc906cc541b528b9f96337c87e2bd710a25ebbd1
RasoulZamani <r.zamani72@gmail.com>
2023-04-01 09:13:53 +0430

    validate serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,6 +4,15 @@ class UserRegisterSerial(serializers.Serializer):
     username = serializers.CharField(required=True)
     email = serializers.EmailField(required=True)
     password= serializers.CharField(required=True, write_only=True)
+    confirm_password = serializers.CharField(required=True, write_only=True)
+    def validate_usrname(self, value):
+        """ filed level validation for username field"""
+        if value =='admin':
+            raise serializers.ValidationError('username cant be admin')
+        return value
     
-    
-    
+    def validate(self, data):
+        """obj level valiation for password confirmation"""
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError('passwords should be equal')
+        return data  
