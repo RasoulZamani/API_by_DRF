deb02e65b9ae2debe5de60307dd0bc64466d346f
RasoulZamani <r.zamani72@gmail.com>
2023-04-01 15:07:42 +0430

    simple model serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,18 +1,34 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
-class UserRegisterSerial(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True)
-    password= serializers.CharField(required=True, write_only=True)
-    confirm_password = serializers.CharField(required=True, write_only=True)
-    def validate_usrname(self, value):
+
+def clean_name(value):
+    if 'fuck' in value:
+        raise serializers.ValidationError('dont be rude!')
+    return value
+
+    
+class UserRegisterSerial(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username','email', 'password')
+        extra_kwargs = {
+            'password': {'write_only':True},
+            'username': {'validators':(clean_name,)},
+        }
+    # username = serializers.CharField(required=True)
+    # email = serializers.EmailField(required=True)
+    # password= serializers.CharField(required=True, write_only=True)
+    # confirm_password = serializers.CharField(required=True, write_only=True)
+    
+    def validate_username(self, value):
         """ filed level validation for username field"""
         if value =='admin':
             raise serializers.ValidationError('username cant be admin')
         return value
     
-    def validate(self, data):
-        """obj level valiation for password confirmation"""
-        if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError('passwords should be equal')
-        return data  
+    # def validate(self, data):
+    #     """obj level valiation for password confirmation"""
+    #     if data['password'] != data['confirm_password']:
+    #         raise serializers.ValidationError('passwords should be equal')
+    #     return data  
