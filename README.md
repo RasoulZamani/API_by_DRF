f3cd954fd373c27edaf6b8c6986feea02e70f2cb
RasoulZamani <r.zamani72@gmail.com>
2023-04-03 15:36:15 +0430

    simple jwt token

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This repo is implementation of DRF consepts throuhg the Mongard course. In my jo
 At first API for registering and creating token implemented, after creating models for Q/A, by `ModelSerializer`, **CRUD** methods was implemented.
 Permisions is handled based on Token Authentication and for more control custom permission file is written.
 CRUD for user model created by ViewSets.
+we used **jwt** token by `djangorestframework_siimplejwt` library.
+
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from rest_framework.authtoken import views as token_views
 from . import views
 from rest_framework import routers
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name='accounts'
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
-    path('api-token-auth/', token_views.obtain_auth_token),
+    #path('api-token-auth/', token_views.obtain_auth_token),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
 
 # router for UserViewSet .../user/...
